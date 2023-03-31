89a2d6a153d2fede36d9638f37f63f77e23efa89
OrelOreo <oreloreopro@gmail.com>
2023-03-31 19:11:51 +0200

    init && routing && debut homepage

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from "../assets/LOGO.png"
+import { Link } from 'react-router-dom'
+import '../Styles/Header.css'
+
+export default function Header() {
+  return (
+    <header>
+        <img src={Logo} alt="Logo Kasa" />
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/about">A Propos</Link>
+        </nav>
+    </header>
+  )
+}
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import '../../Styles/Home.css'
+
+export default function Home() {
+  return (
+    <div>
+        <Header />
+        <div className='banner'>
+            <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+        <section>
+            <div>TOTO</div>
+        </section>
+    </div>
+  )
+}
