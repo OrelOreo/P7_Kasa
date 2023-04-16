8b3c5baa79894997009a74c579be1d29aff4aa8e
OrelOreo <oreloreopro@gmail.com>
2023-04-16 16:57:22 +0200

    data dynamiques dans chaque page produit

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -13,9 +13,9 @@ export default function Card() {
           return (
             <Link key={ item.id } to={{
               pathname: `./${item.id}`
-            }}>
+            }} state={ item }>
               <div className='card'>
-                <img src={item.cover} alt="location d'appartement" />
+                <img src={ item.cover } alt="location d'appartement" />
                 <h3> { item.title }</h3>
               </div>
             </Link>
--- src/Pages/PageProduit/PageProduit.js
+++ src/Pages/PageProduit/PageProduit.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from "../../Components/Header/Header"
 import Footer from "../../Components/Footer/Footer"
+import Star from "../../assets/star.png"
+import './PageProduit.css'
 
 export default function PageProduit() {
+  const location = useLocation()
+  const tags = location.state.tags
+
   return (
     <div>
         <Header />
+        <div className="card-content">
+          <div className="caroussel">
+            <img src={ location.state.cover } alt="" />
+          </div>
+          <div className="container-infos">
+            <div className="location">
+              <h2>{ location.state.title }</h2>
+              <p>{ location.state.location }</p>
+            </div>
+            <div className="profil">
+              <p> { location.state.host.name }</p>
+              <img src={ location.state.host.picture} alt="profil du vendeur"/>
+            </div>
+          </div>
+          <div className="container-tags-ratings">
+            <div className="tags">
+              {
+                tags.map((tag, index) => {
+                  return (
+                    <p className='tag' key={index}>{ tag }</p>
+                  )
+                })
+              }
+            </div>
+            <div className="ratings">
+
+            </div>
+          </div>
+        </div>
         <Footer />
     </div>
   )
